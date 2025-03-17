0396096f7cb2f097e24ae9631e04cb1c20d6d9d5
behemoth-thainv <thainv@behemoth.vn>
2025-03-17 16:10:56 +0700

    Update config

---
+++ golang/config/initializers.config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"os"
+
+	"github.com/BehemothLtd/behemoth-pkg/golang/loggers"
+	"github.com/BehemothLtd/behemoth-pkg/golang/utils"
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func InitEnvironment(appName string) {
+	LoadEnv()
+	InitLogger(appName)
+}
+
+func LoadEnv() {
+	env := os.Getenv("APP_ENV")
+	if env == "" {
+		env = "development"
+	}
+
+	if env == "development" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Info().Msg("No .env file found, using default values")
+		}
+	}
+}
+
+func InitLogger(appName string) {
+	zerolog.SetGlobalLevel(loggers.GetLogLevel())
+
+	appEnv := utils.GetEnv("APP_ENV", "development")
+
+	if utils.GetEnv("APP_DEBUG", "false") == "true" {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	if utils.IsLocalEnv() {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	} else {
+		writer := &loggers.InfoDebugWriter{
+			Stdout: os.Stdout,
+			Stderr: os.Stderr,
+		}
+
+		log.Logger = zerolog.New(writer).With().Str("app", appName).Timestamp().Caller().Logger()
+	}
+	log.Logger = log.Logger.Hook(loggers.TracingHook{})
+
+	log.Logger.Debug().Str("logLevel", zerolog.GlobalLevel().String()).Str("appEnv", appEnv).Msg("🚀 Logger initialized")
+}
