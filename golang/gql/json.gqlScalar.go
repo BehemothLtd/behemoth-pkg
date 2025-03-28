e2f4f6d680d45b97871293a48f0241433cff2d6b
thangnt-behemoth <thangnt@behemoth.vn>
2025-03-28 15:57:56 +0700

    fix UnmarshalGraphQL to handle gql json input

--- golang/gql/json.gqlScalar.go
+++ golang/gql/json.gqlScalar.go
@@ -15,11 +15,21 @@ func (JSON) ImplementsGraphQLType(name string) bool {
 
 func (j *JSON) UnmarshalGraphQL(input interface{}) error {
 	switch input := input.(type) {
-	case JSON:
-		*j = input
+	case map[string]interface{}, []interface{}:
+		data, err := json.Marshal(input)
+		if err != nil {
+			return err
+		}
+		*j = JSON(data)
+		return nil
+	case string:
+		*j = JSON([]byte(input))
+		return nil
+	case nil:
+		*j = nil
 		return nil
 	default:
-		return fmt.Errorf("wrong type")
+		return fmt.Errorf("invalid JSON input type: %T", input)
 	}
 }
 
